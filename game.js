a70ac342860fca14766c50a252bca83c575fde49
Juiin <dadude000@gmail.com>
2024-01-29 21:45:27 +0100

    Button only on current round

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
 solution = [1, 2, 3, 4];
-currentRound = 1;
+currentRound = 0;
+maxAttempts = 10;
 
 numberElements = [];
-for (let xx = 0; xx < 10; xx++) {
+for (let xx = 0; xx < maxAttempts; xx++) {
     numberElements[xx] = [];
     for(let yy = 0; yy < 4; yy++){
         numberElements[xx][yy] = document.getElementById("round" + xx + "Number" + yy);        
     }
 }
+
+confirmButtons = [];
+for(let i=0; i<maxAttempts; i++){
+    confirmButtons[i] = document.getElementById("button" + i);
+}
+
+confirmButtons[currentRound].style.visibility = "visible";
 
 for(let i=0; i<4; i++){
     numberElements[currentRound][i].placeholder = "";
