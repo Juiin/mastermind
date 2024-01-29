279197328f12e00ca006040a2d26a9f5d1295bc4
Juiin <dadude000@gmail.com>
2024-01-29 22:04:33 +0100

    Advancing rounds with button press

--- game.js
+++ game.js
@@ -13,12 +13,36 @@ for (let xx = 0; xx < maxAttempts; xx++) {
 confirmButtons = [];
 for(let i=0; i<maxAttempts; i++){
     confirmButtons[i] = document.getElementById("button" + i);
+    confirmButtons[i].onclick = function(){
+        //If any of the current playable fields ar empty return out of event
+        for(let i=0;i<4;i++){
+            if(numberElements[currentRound][i].value == ""){
+                return;
+            }
+        }
+        
+        currentRound++;
+        newRound();
+    }
 }
 
-confirmButtons[currentRound].style.visibility = "visible";
+function newRound(){
+    //Lock old row if its not the first
+    if(currentRound > 0){
+        confirmButtons[currentRound-1].style.visibility = "hidden";
+        for(let i=0; i<4; i++){
+            numberElements[currentRound-1][i].readOnly = true;
+        }
+    }
+    
+    //Make current row playable
+    confirmButtons[currentRound].style.visibility = "visible";
 
-for(let i=0; i<4; i++){
-    numberElements[currentRound][i].placeholder = "";
-    numberElements[currentRound][i].removeAttribute("readonly")
-
+    for(let i=0; i<4; i++){
+        numberElements[currentRound][i].placeholder = "";
+        numberElements[currentRound][i].removeAttribute("readonly")
+    }
 }
+
+
+newRound();
